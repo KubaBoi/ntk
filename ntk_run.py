d5b43ef0d597762631367b6591320c16e70bc137
Jakub Anderle <jakub@kuba.kuba>
2023-01-06 16:34:03 +0100

    'Auto Update'

--- ntk_run.py
+++ ntk_run.py
@@ -85,7 +85,7 @@ while (True):
         print("Commit...")
         subprocess.call(["git", "commit", "-m 'Auto Update'"])
         print("Push...")
-        subprocess.call(["git", "push", "< ./token"])
+        subprocess.call(["git", "push", "< token"])
         print("Waiting 3 minutes...")
         time.sleep(180)
     time.sleep(20)
